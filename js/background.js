0bb9c56532d64899dfd73d5e9892dc3c45762ddc
Xwilarg <xwilarg@yahoo.fr>
2018-09-07 00:53:36 +0200

    Icon now is grey out outside of nhentai doujinshi

--- js/background.js
+++ js/background.js
@@ -1,3 +1,12 @@
+chrome.tabs.onUpdated.addListener(function
+    (tabId, changeInfo, tab) {
+        if (changeInfo.url.match('https://nhentai.net/g/[0-9]*/[/0-9a-z]*'))
+            chrome.browserAction.setIcon({path: "Icon.png"});
+        else
+            chrome.browserAction.setIcon({path: "Icon-grey.png"});
+    }
+);
+
 function download() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         let url = tabs[0].url;
