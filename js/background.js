9334181fbd2bdcf88885b0f4d3fee90b0f42ce29
Xwilarg <xwilarg@yahoo.fr>
2018-09-08 00:16:54 +0200

    Fix icon not being grey at tab switch

--- js/background.js
+++ js/background.js
@@ -1,13 +1,22 @@
 chrome.tabs.onUpdated.addListener(function
     (tabId, changeInfo, tab) {
-        if (changeInfo.url === undefined)
-            ;
-        else if (changeInfo.url.match('https://nhentai.net/g/[0-9]*/[/0-9a-z]*'))
-            chrome.browserAction.setIcon({path: "Icon.png"});
-        else
-            chrome.browserAction.setIcon({path: "Icon-grey.png"});
+        if (changeInfo.url !== undefined)
+            setIcon(changeInfo.url);
     }
 );
+
+chrome.tabs.onActivated.addListener(function() {
+    chrome.tabs.getSelected(null,function(tab) {
+        setIcon(tab.url);
+    });
+});
+
+function setIcon(url) {
+    if (url.match('https://nhentai.net/g/[0-9]*/[/0-9a-z]*'))
+        chrome.browserAction.setIcon({path: "Icon.png"});
+    else
+        chrome.browserAction.setIcon({path: "Icon-grey.png"});
+}
 
 function download() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
