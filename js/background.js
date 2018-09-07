77fb51797343b663df94584136262922a7bb3b06
Xwilarg <xwilarg@yahoo.fr>
2018-09-07 18:53:17 +0200

    Fix url being undefined

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
 chrome.tabs.onUpdated.addListener(function
     (tabId, changeInfo, tab) {
-        if (changeInfo.url.match('https://nhentai.net/g/[0-9]*/[/0-9a-z]*'))
+        if (changeInfo.url === undefined)
+            ;
+        else if (changeInfo.url.match('https://nhentai.net/g/[0-9]*/[/0-9a-z]*'))
             chrome.browserAction.setIcon({path: "Icon.png"});
         else
             chrome.browserAction.setIcon({path: "Icon-grey.png"});
