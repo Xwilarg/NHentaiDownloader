35a155535e45a39dfc1b452dde9587389f0390fd
Xwilarg <xwilarg@yahoo.fr>
2018-09-09 12:05:54 +0200

    Can now download inside a folder

--- js/background.js
+++ js/background.js
@@ -18,34 +18,30 @@ function setIcon(url) {
         chrome.browserAction.setIcon({path: "Icon-grey.png"});
 }
 
-function download() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        let url = tabs[0].url;
-        if (url.match('https://nhentai.net/g/[0-9]*/[/0-9a-z]*')) {
-            let id = url.replace("https://nhentai.net/g/", "").split('/')[0];
-            let http = new XMLHttpRequest();
-            http.onreadystatechange = function() {
-                if (this.readyState === 4) {
-                    if (this.status === 200) {
-                        let json = JSON.parse(this.responseText);
-                        let mediaId = json.media_id;
-                        for (let page in json.images.pages)
-                        {
-                            let format = (json.images.pages[page].t === 'j') ? ('.jpg') : ('.png');
-                            chrome.downloads.download({
-                                url: 'https://i.nhentai.net/galleries/' + mediaId + '/' + (parseInt(page) + 1) + format
-                            });
-                        }
-                    } else {
-                        document.getElementById('action').innerHTML = "An unexpected error occured (Code " + this.status + ").";
-                    }
-                }
-            };
-            http.open("GET", 'https://nhentai.net/api/gallery/' + id, true);
-            http.send();
+function download(url, path) {
+    if (url.match('https://nhentai.net/g/[0-9]*/[/0-9a-z]*')) {
+        let id = url.replace("https://nhentai.net/g/", "").split('/')[0];
+        let http = new XMLHttpRequest();
+        http.open("GET", 'https://nhentai.net/api/gallery/' + id, false);
+        http.send();
+        if (http.status === 200) {
+            let json = JSON.parse(http.responseText);
+            let mediaId = json.media_id;
+            for (let page in json.images.pages)
+            {
+                let format = (json.images.pages[page].t === 'j') ? ('.jpg') : ('.png');
+                chrome.downloads.download({
+                    url: 'https://i.nhentai.net/galleries/' + mediaId + '/' + (parseInt(page) + 1) + format,
+                    filename: './' + path + '/' + (parseInt(page) + 1) + format
+                });
+            }
+            return ("Your files are downloading, thanks for using NHentaiDownloader.");
+        } else if (http.status === 403) {
+            return ("This extension must be used on a doujinshi page in nhentai.net.");
+        } else {
+            return ("An unexpected error occured (Code " + http.status + ").");
         }
-        else
-            document.getElementById('action').innerHTML = "This extension must be used on a doujinshi page in nhentai.net.";
-    });
-    document.getElementById('action').innerHTML = "Your files are downloading, thanks for using NHentaiDownloader.";
+    }
+    else
+        return ("This extension must be used on a doujinshi page in nhentai.net.");
 }
--- js/preview.js
+++ js/preview.js
@@ -7,8 +7,12 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             if (this.readyState === 4) {
                 if (this.status === 200) {
                     let json = JSON.parse(this.responseText);
-                    document.getElementById('action').innerHTML = '<h3>' + json.title.pretty + '</h3><div id="center">(' + json.images.pages.length + ' pages)</div><br/><input type="button" id="button" value="Download">';
-                    document.getElementById('button').addEventListener('click', chrome.extension.getBackgroundPage().download);
+                    document.getElementById('action').innerHTML = '<h3>' + json.title.pretty + '</h3><div id="center">(' + json.images.pages.length + ' pages)' +
+                    '</div><br/><input type="button" id="button" value="Download"><br/><br/>Downloads/<input type="text" id="path">';
+                    document.getElementById('button').addEventListener('click', function()
+                    {
+                        document.getElementById('action').innerHTML = chrome.extension.getBackgroundPage().download(url, document.getElementById('path').value); 
+                    });
                 } else {
                     document.getElementById('action').innerHTML = "An unexpected error occured (Code " + this.status + ").";
                 }
