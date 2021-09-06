ae682feb4e7e94a331a181ba06bb225bcc92470c
Xwilarg <xwilarg@yahoo.fr>
2021-09-06 21:11:00 +0200

    Add multiple download

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,6 @@
+import ApiParsing from "../parsing/ApiParsing";
 import Downloader from "./Downloader";
+import { utils } from "../utils/utils";
 var JSZip = require("jszip");
 
 chrome.tabs.onUpdated.addListener(function
@@ -27,6 +29,7 @@ function setIcon(url: string) {
 module background
 {
     let currentDownloader: Downloader | null = null;
+    let parsing = new ApiParsing();
 
     export function isDownloadFinished(): boolean {
         return currentDownloader == null || currentDownloader.isDone();
@@ -36,6 +39,63 @@ module background
         let zip = new JSZip();
         currentDownloader = new Downloader(jsonTmp, path, errorCallback, progressCallback, name, zip, true);
         currentDownloader.startAsync();
+    }
+
+    export function downloadAllDoujinshis(allDoujinshis: Record<string, string>, finalName: string, errorCallback: Function, progressCallback: Function) {
+        downloadAllDoujinshisAsync(allDoujinshis, finalName, errorCallback, progressCallback);
+    }
+
+    async function downloadAllDoujinshisAsync(allDoujinshis: Record<string, string>, finalName: string, errorCallback: Function, progressCallback: Function) {
+        let downloadName: string = "";
+        let duplicateBehaviour: string = "";
+        let replaceSpaces: boolean = false;
+        await new Promise((resolve, _reject) => {
+            resolve(
+                chrome.storage.sync.get({
+                    downloadName: "{pretty}",
+                    duplicateBehaviour: "remove",
+                    replaceSpaces: true
+                }, function(elems) {
+                    downloadName = elems.downloadName;
+                    duplicateBehaviour = elems.duplicateBehaviour;
+                    replaceSpaces = elems.replaceSpaces;
+                })
+            );
+        });
+        let zip = new JSZip();
+        let names: Array<string> = [];
+        let length = Object.keys(allDoujinshis).length;
+        let allKeys = Object.keys(allDoujinshis);
+
+        for (let i = 0; i < length; i++) {
+            let key = allKeys[i];
+            const resp = await fetch(parsing.GetUrl(key));
+            if (resp.ok)
+            {
+                const json = await parsing.GetJsonAsync(resp);
+
+                let title = utils.getDownloadName(downloadName, json.title.pretty === "" ?
+                    json.title.english.replace(/\[[^\]]+\]/g, '').replace(/\([^\)]+\)/g, '') : json.title.pretty,
+                    json.title.english, json.title.japanese, key, json.tags);
+                if (duplicateBehaviour == "remove") {
+                    let c = 2;
+                    let tmp = title;
+                    while (names.includes(tmp)) {
+                        tmp = title + " (" + c + ")";
+                        c++;
+                    }
+                    title = tmp;
+                    names.push(title);
+                }
+                zip.folder(utils.cleanName(title, replaceSpaces));
+                currentDownloader = new Downloader(json, utils.cleanName(title, replaceSpaces), errorCallback, progressCallback, allDoujinshis[key], zip, i < length - 1);
+                await currentDownloader.startAsync();
+            }
+            else
+            {
+                errorCallback("Can't download " + key + " (Code " + resp.status + ": " + resp.statusText + ").");
+            }
+        }
     }
 
     export function goBack() {
@@ -54,6 +114,8 @@ module background
 window.isDownloadFinished = background.isDownloadFinished;
 // @ts-ignore
 window.downloadDoujinshi = background.downloadDoujinshi;
+// @ts-ignore
+window.downloadAllDoujinshis = background.downloadAllDoujinshis;
 // @ts-ignore
 window.goBack = background.goBack;
 // @ts-ignore
