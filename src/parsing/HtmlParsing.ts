33b12f2877c74b4513a8cda097097a783ca74805
Xwilarg <xwilarg@yahoo.fr>
2021-09-07 04:22:51 +0200

    Fix HTML parsing not working

--- src/parsing/HtmlParsing.ts
+++ src/parsing/HtmlParsing.ts
@@ -7,6 +7,15 @@ export default class HtmlParsing implements AParsing
     }
 
     GetJsonAsync(response: Response): Promise<any> {
-        return response.text().then((value: string) => { return value.split("gallery: ")[1].split(",\n\t\t\tstart_page:")[0]; });
+        return response.text().then((value: string) =>
+        {
+            return JSON.parse(
+                value.split("window._gallery = JSON.parse(\"")[1].split("\");")[0].replace(/\\u[\dA-F]{4}/gi,
+                    function (match) {
+                         return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+                    }
+                )
+            );
+        });
     }
 }
